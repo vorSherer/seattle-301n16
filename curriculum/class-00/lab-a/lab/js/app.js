41fcf164a19dfac237d82a80aa42e0d98ecd11d2
Thomas Sherer <olc0710@yahoo.com>
2020-01-04 13:28:06 -0800

    Answered the TODO question for 'let' substitution.

--- curriculum/class-00/lab-a/lab/js/app.js
+++ curriculum/class-00/lab-a/lab/js/app.js
@@ -37,6 +37,7 @@ function displayPics(){
   }
   console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
+  //A ReferenceError is thrown by 'let' because the let-declared variable is in a Temporal Dead Zone.
   // PUT YOUR RESPONSE IN THIS COMMENT
   console.log(viewed);
 
