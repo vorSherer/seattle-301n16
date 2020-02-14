90356721d3eda4ed52befc5a35342c48588f8565
Thomas Sherer <olc0710@yahoo.com>
2020-02-14 14:41:18 -0800

    Step 10 line 156 - code broken.

--- curriculum/class-00/lab-b/lab/app.js
+++ curriculum/class-00/lab-b/lab/app.js
@@ -153,13 +153,11 @@ console.log(joe);
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
-Student.prototype.greeting = function() {
-  return `Hi, my name is ${this.name}`;
-};
+Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
